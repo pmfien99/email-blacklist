d2ff137a6eddbfe46032fbd1454cf7c6ff2a8758
Patrick Fien <73155518+pmfien99@users.noreply.github.com>
2023-07-19 16:54:02 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import blacklist from './utils/blacklist';
+
+window.Webflow ||= [];
+window.Webflow.push(() => {
+  const masterBlacklist: string[] = blacklist;
+
+  const emailSubmit: HTMLElement | null = document.getElementById('demoFormSubmit');
+  const emailField: HTMLInputElement | null = document.getElementById('email') as HTMLInputElement;
+
+  if (emailSubmit && emailField) {
+    emailSubmit.addEventListener('click', () => {
+      const domain: string = emailField.value.split('@')[1];
+
+      if (masterBlacklist.indexOf(domain) !== -1) {
+        emailField.value = '';
+        emailField.setAttribute(
+          'placeholder',
+          'Enter a valid business email or contact us at hello@daloopa.com.'
+        );
+      }
+    });
+  }
+});
